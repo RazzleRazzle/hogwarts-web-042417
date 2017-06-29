e2ee6567bdfc5a208cef3440539b65d38eb952f9
Jake Musich <makejusich@gmail.com>
2017-06-29 11:32:13 -0400

    more

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,16 +3,19 @@ import React from 'react'
 export default class Card extends React.Component {
 
 	getImageURL() {
-		return this.props.image
+			var image = (this.props.image)
+			console.log(image)
+
+		return image
 	}
 
 	render() {
 
 		return(
 			<div className="card">
-				<img src={require(this.getImageURL())} alt={this.props.name}/>
+				<img src={this.getImageURL()} alt={this.props.name}/>
 				<div className="description">
-
+					{this.props.name}
 				</div>
 			</div>
 		)
